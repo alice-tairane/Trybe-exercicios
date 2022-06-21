2b45181e642425bd811659710faf8109f441cc14
Alice Tairane <alicetairane77@gmail.com>
2022-06-21 17:40:26 -0300

    adiciona variaveis e constantes

---
+++ fundamentos/Bloco4/script.js
@@ -0,0 +1,14 @@
+const myName = "Alice ";
+const birthCity = " Santa Barbara" ;
+let birthYear =2002;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030 ;
+
+console.log(birthYear);
+
+ birthCity ="feira";
+ console.log(birthCity);
